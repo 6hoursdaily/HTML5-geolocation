0777f83cc92ae089fdb419d4d7c9a4be1d02412e
Jerry Pascua <jerrypascua@gmail.com>
2012-11-11 16:03:43 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gmaps::Application.config.secret_token = '602d16747b18feba71aef6d4c94c12057090ee92311ec9e4233908148cc2069a5266ccb5b4cc9690a723152d797d6433edbc5c72d39cad0b84261e48e4935af5'
