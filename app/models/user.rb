f8569f09bb7cfd71be9baa43b361e51f2e4d0cf3
Jerry Pascua <jerrypascua@gmail.com>
2012-11-11 17:16:36 +0800

    added gmaps gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  acts_as_gmappable
+
+  def gmaps4rails_address
+    #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
+
+    "#{self.street}, #{self.city}, #{self.country}" 
+
+  end
 end
