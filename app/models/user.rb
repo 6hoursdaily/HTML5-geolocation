fbfc03095cb612518b36d4fb1f433ff5b3e9176d
Jerry Pascua <jerrypascua@gmail.com>
2012-11-21 03:17:50 +0800

    replaced gmaps4rails with HTML5

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,4 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
-  acts_as_gmappable
-
-  def gmaps4rails_address
-    #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
-
-    "#{self.street}, #{self.city}, #{self.country}" 
-
-  end
 end
