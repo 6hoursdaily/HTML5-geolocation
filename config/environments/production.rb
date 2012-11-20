7a5f80ee0e98fe16c5901b8ee8b144ead783af93
Jerry Pascua <jerrypascua@gmail.com>
2012-11-21 01:00:52 +0800

    prevent initialize on precompile for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,9 @@ Gmaps::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( gmaps.js )
 
+  # HEROKU prevent initialize on precompile
+  config.assets.initialize_on_precompile = false
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
